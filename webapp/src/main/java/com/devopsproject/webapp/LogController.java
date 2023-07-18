3a2a3916a29b7907d93e9563ac9f802e3b2ec3c3
Or Itskovich <oritskovich26@gmail.com>
2023-07-18 09:23:37 +0300

    Post-mapping to add new logs to database

--- webapp/src/main/java/com/devopsproject/webapp/LogController.java
+++ webapp/src/main/java/com/devopsproject/webapp/LogController.java
@@ -4,6 +4,8 @@ package com.devopsproject.webapp;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -14,6 +16,10 @@ import java.util.List;
 public class LogController {
     private LogRepo logRepo;
 
+    @PostMapping("/logs")
+    Log createLog(@RequestBody Log newLog){
+        return logRepo.save(newLog);
+    }
     @GetMapping("**")
     public String saveLog(HttpServletRequest request) {
         String route = request.getRequestURI();
