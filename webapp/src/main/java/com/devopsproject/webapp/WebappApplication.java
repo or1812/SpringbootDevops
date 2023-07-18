d0136520e60967132d5c19c0e3544958be1a5550
Or Itskovich <112501462+or1812@users.noreply.github.com>
2023-07-18 17:26:32 +0300

    Update WebappApplication.java

--- webapp/src/main/java/com/devopsproject/webapp/WebappApplication.java
+++ webapp/src/main/java/com/devopsproject/webapp/WebappApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebappApplication {
 
+	//  The main method that starts the Spring Boot application.
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
