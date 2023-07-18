9702626b4ca2ead735ac9c685a2a6bcde4744e07
Or Itskovich <112501462+or1812@users.noreply.github.com>
2023-07-18 17:23:58 +0300

    Update LogRepo.java

--- webapp/src/main/java/com/devopsproject/webapp/LogRepo.java
+++ webapp/src/main/java/com/devopsproject/webapp/LogRepo.java
@@ -3,6 +3,8 @@ package com.devopsproject.webapp;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+ // A Spring Data JPA repository interface for managing Log entities, it provides basic CRUD operations for the Log entity.
 @Repository
 public interface LogRepo extends JpaRepository<Log, Long> {
 }
