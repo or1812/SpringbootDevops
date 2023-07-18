05f9c5c52f779047b8ecafabe66a6aae56a566b3
Or Itskovich <oritskovich26@gmail.com>
2023-07-18 16:59:36 +0300

    Added python unit tests to check the functionality of the web application.

--- webapp/src/main/java/com/devopsproject/webapp/SeedDB.java
+++ webapp/src/main/java/com/devopsproject/webapp/SeedDB.java
@@ -2,15 +2,14 @@ package com.devopsproject.webapp;
 
 
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 @RequestMapping("/")
@@ -19,8 +18,10 @@ public class SeedDB {
     @Bean
     CommandLineRunner seed(LogRepo logRepo){
         return args -> {
-            LocalDateTime timestamp = LocalDateTime.now();
-            String seedMessage = "Seeding made on " + timestamp;
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+            String formattedDateTime = currentDateTime.format(formatter);
+            String seedMessage = "Seeding made on " + formattedDateTime;
             Log log = new Log(seedMessage);
             logger.info("Saving seed log to database... ");
             logRepo.save(log);
